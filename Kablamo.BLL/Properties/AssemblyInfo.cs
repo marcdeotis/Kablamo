b2f71eb434f24b41d0b0503a2d74ed31b05aa83b
Marc DeOtis <marc@deotis.com>
2017-06-08 14:50:04 -0500

    Updating properties and assembly info name to Kablamo

--- Kablamo.BLL/Properties/AssemblyInfo.cs
+++ Kablamo.BLL/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("GameProject.BLL")]
+[assembly: AssemblyTitle("Kablamo.BLL")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("GameProject.BLL")]
+[assembly: AssemblyProduct("Kablamo.BLL")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
--- Kablamo.UI/Properties/AssemblyInfo.cs
+++ Kablamo.UI/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("GameProject.UI")]
+[assembly: AssemblyTitle("Kablamo.UI")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("GameProject.UI")]
+[assembly: AssemblyProduct("Kablamo.UI")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
