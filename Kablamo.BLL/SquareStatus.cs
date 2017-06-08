ee5b96649d5187794daea4a0118cc5118434a81e
Jordan Yemma <jordanyemma@gmail.com>
2017-06-08 14:45:12 -0500

    Resubmitting Kablamo

---
+++ Kablamo.BLL/SquareStatus.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kablamo.BLL
+{
+    public enum SquareStatus
+    {
+        Empty,
+        Wall,
+        RedBase,
+        BlueBase,
+        SingleBlue,
+        DoubleBlue,
+        SingleRed,
+        DoubleRed,
+
+    }
+}
