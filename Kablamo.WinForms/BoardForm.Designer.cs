6ed921cd92c11759dcc2534bdf41664f18ed81d4
Marc DeOtis <marc@deotis.com>
2017-06-08 23:58:57 -0500

    Addition of WinForm project to solution Getting new board class during form construction Created method for autogen of cols/rows Populates each cell with color based on current status of the board

---
+++ Kablamo.WinForms/BoardForm.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace Kablamo.WinForms
+{
+    partial class BoardForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gameTable = new System.Windows.Forms.TableLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // gameTable
+            // 
+            this.gameTable.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Single;
+            this.gameTable.ColumnCount = 2;
+            this.gameTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.gameTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.gameTable.Location = new System.Drawing.Point(50, 50);
+            this.gameTable.Margin = new System.Windows.Forms.Padding(0, 0, 0, 0);
+            this.gameTable.Name = "gameTable";
+            this.gameTable.RowCount = 2;
+            this.gameTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 49F));
+            this.gameTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 51F));
+            this.gameTable.Size = new System.Drawing.Size(500, 500);
+            this.gameTable.TabIndex = 0;
+            // 
+            // BoardForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(610, 591);
+            this.Controls.Add(this.gameTable);
+            this.Name = "BoardForm";
+            this.Text = "Kablamo";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel gameTable;
+    }
+}
---
+++ Kablamo.WinForms/BoardForm.cs
@@ -0,0 +1,109 @@
+﻿using Kablamo.BLL;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kablamo.WinForms
+{
+    public partial class BoardForm : Form
+    {
+        public BoardForm()
+        {
+            InitializeComponent();
+
+            _gameBoard = new Board();
+
+            FormatTableLayout();
+        }
+
+        private void FormatTableLayout()
+        {
+            minX = _gameBoard.BoardState.Select(s => s.Key.XCoordinate).Min();
+            maxX = _gameBoard.BoardState.Select(s => s.Key.XCoordinate).Max();
+            minY = _gameBoard.BoardState.Select(s => s.Key.YCoordinate).Min();
+            maxY = _gameBoard.BoardState.Select(s => s.Key.YCoordinate).Max();
+
+            xRowCount = Math.Abs(minX - maxX) + 1;
+            yColCount = Math.Abs(minY - maxY) + 1;
+
+            gameTable.ColumnStyles.Clear();
+            gameTable.RowStyles.Clear();
+
+            gameTable.ColumnCount = yColCount;
+            gameTable.RowCount = xRowCount;
+
+            for(int count = 0; count < yColCount; count++)
+            {
+                gameTable.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, yColCount / 100f));
+            }
+
+            for (int count = 0; count < xRowCount; count++)
+            {
+                gameTable.RowStyles.Add(new RowStyle(SizeType.Percent, xRowCount / 100f));
+            }
+
+            foreach(var coord in _gameBoard.BoardState)
+            {
+                Color squareColor;
+
+                switch (coord.Value)
+                {
+                    case SquareStatus.BlueBase:
+                        squareColor = Color.Blue;
+                        break;
+                    case SquareStatus.DoubleBlue:
+                        squareColor = Color.FromArgb(100, 100, 255);
+                        break;
+                    case SquareStatus.DoubleRed:
+                        squareColor = Color.FromArgb(255, 100, 100);
+                        break;
+                    case SquareStatus.Empty:
+                        squareColor = Color.White;
+                        break;
+                    case SquareStatus.RedBase:
+                        squareColor = Color.Red;
+                        break;
+                    case SquareStatus.SingleBlue:
+                        squareColor = Color.FromArgb(200, 200, 255);
+                        break;
+                    case SquareStatus.SingleRed:
+                        squareColor = Color.FromArgb(255, 200, 200);
+                        break;
+                    case SquareStatus.Wall:
+                        squareColor = Color.Black;
+                        break;
+                    default:
+                        squareColor = Color.White;
+                        break;
+                }
+
+                gameTable.Controls.Add(
+                    new Panel()
+                    { BackColor = squareColor,
+                        Dock = DockStyle.Fill,
+                        Margin = new Padding(0) },
+                    coord.Key.YCoordinate,
+                    coord.Key.XCoordinate);
+            }
+
+
+
+
+
+        }
+
+        Board _gameBoard;
+        int minX;
+        int maxX;
+        int minY;
+        int maxY;
+        int xRowCount;
+        int yColCount;
+    }
+}
